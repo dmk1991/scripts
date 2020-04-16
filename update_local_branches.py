6aaabeafacbae26957bac72ff8d6ff46c8b1c7ed
David <david.kreuzburg@cisecurity.org>
2020-04-16 13:29:16 -0400

    Update local git branches script

---
+++ update_local_branches.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import os
+
+def print_spacer():
+	print(f"{'-'*10}\n")
+
+
+def main():
+	cmd = 'git branch'
+	stream = os.popen(cmd)
+
+	for branch in stream.readlines():
+		formatted_branch = branch.replace('* ', '').strip()
+		pull_cmd = f'git checkout {formatted_branch} && git pull'
+		pull_stream = os.popen(pull_cmd)
+		
+		for line in pull_stream.readlines():
+			print(line)
+
+		print_spacer()
+
+main()
