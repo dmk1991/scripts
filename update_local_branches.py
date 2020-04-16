5f096b1b83eaf167b7c33e436f26a8046efcd3df
David <david.kreuzburg@cisecurity.org>
2020-04-16 13:43:59 -0400

    PEP-8 formatter

--- update_local_branches.py
+++ update_local_branches.py
@@ -3,21 +3,21 @@
 import os
 
 def print_spacer():
-	print(f"{'-'*10}\n")
+    print(f"{'-'*10}\n")
 
 
 def main():
-	cmd = 'git branch'
-	stream = os.popen(cmd)
+    cmd = 'git branch'
+    stream = os.popen(cmd)
 
-	for branch in stream.readlines():
-		formatted_branch = branch.replace('* ', '').strip()
-		pull_cmd = f'git checkout {formatted_branch} && git pull'
-		pull_stream = os.popen(pull_cmd)
-		
-		for line in pull_stream.readlines():
-			print(line)
+    for branch in stream.readlines():
+        formatted_branch = branch.replace('* ', '').strip()
+        pull_cmd = f'git checkout {formatted_branch} && git pull'
+        pull_stream = os.popen(pull_cmd)
 
-		print_spacer()
+        for line in pull_stream.readlines():
+            print(line)
+
+        print_spacer()
 
 main()
