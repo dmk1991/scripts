60f30785ddd41b6878cdca6adb1fb9e59201252e
David <david.kreuzburg@cisecurity.org>
2020-04-17 09:25:41 -0400

    Updates update_local_branches to use list comprehension

--- update_local_branches.py
+++ update_local_branches.py
@@ -6,18 +6,22 @@ def print_spacer():
     print(f"{'-'*10}\n")
 
 
+def update_branch(branch):
+    formatted_branch = branch.replace('* ', '').strip()
+    pull_cmd = f'git checkout {formatted_branch} && git pull'
+    pull_stream = os.popen(pull_cmd)
+
+    for line in pull_stream.readlines():
+        print(line)
+
+    print_spacer()
+
+
 def main():
     cmd = 'git branch'
     stream = os.popen(cmd)
 
-    for branch in stream.readlines():
-        formatted_branch = branch.replace('* ', '').strip()
-        pull_cmd = f'git checkout {formatted_branch} && git pull'
-        pull_stream = os.popen(pull_cmd)
+    [update_branch(branch) for branch in stream.readlines()]
 
-        for line in pull_stream.readlines():
-            print(line)
-
-        print_spacer()
 
 main()
