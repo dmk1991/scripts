7d764f3a014c16876baab5adeebeac976248390a
David <david.kreuzburg@cisecurity.org>
2021-04-19 15:58:30 -0400

    Add refactor comments to update_local_branches

--- update_local_branches.py
+++ update_local_branches.py
@@ -7,6 +7,11 @@ def print_spacer():
 
 
 def update_branch(branch):
+    # refactor idea for OOP methodology
+    # use factory to determine method
+    # factory uses branch name to determine method
+    # if branch name starts with * strip it use checkout method
+    # if branch name does not start with * use fetch origin method
     formatted_branch = branch.replace('* ', '').strip()
     pull_cmd = f'git fetch origin {formatted_branch}:{formatted_branch}'
     pull_stream = os.popen(pull_cmd)
